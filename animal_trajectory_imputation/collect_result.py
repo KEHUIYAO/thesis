0e9a55e8ccacde7f1afe73b95714a96729ef5116
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2024-05-16 22:35:02 -0500

    modify the collect_result.py, it can now output the mae for different missing ratios

--- animal_trajectory_imputation/collect_result.py
+++ animal_trajectory_imputation/collect_result.py
@@ -1,85 +1,94 @@
 import os
 import re
 import pandas as pd
-deer_id_list = os.listdir('./results/')
-
-# remove .DS_Store
-deer_id_list = [int(x) for x in deer_id_list if x != '.DS_Store']
-deer_list = []
-mae_interpolation_list = []
-mae_transformer_list = []
-mae_csdi_list = []
-num_eval_points_list = []
-crps_list = []
-
-for deer_id in sorted(deer_id_list):
-    # print(deer_id)
-
-    # enter the interpolation folder, and read mae.txt file
-    try:
-        with open(f'./results/{deer_id}/interpolation/mae.txt') as f:
-            text = f.read()
-
-            # extract the mae value from 'Test MAE: 123.456\nTest MRE' using regex
-            specific_number = re.search(r'Test MAE: (\d+\.\d+|\d+)', text)
-            mae_interpolation = float(specific_number.group(1)) if specific_number else None
 
 
-        # with open(f'./results/{deer_id}/transformer/mae.txt') as f:
-        #     mae = f.read()
-        #     print(mae)
+p_missing_list = [20, 50, 80]
 
-        with open(f'./results/{deer_id}/csdi/mae.txt') as f:
-            text = f.read()
-            # extract the mae value from 'Test MAE: 123.456\nTest MRE' using regex
-            specific_number = re.search(r'Test MAE: (\d+\.\d+|\d+)', text)
-            mae_csdi = float(specific_number.group(1)) if specific_number else None
-            num_eval_points = re.search(r'Number of evaluated data points: (\d+)', text)
-            num_eval_points = int(num_eval_points.group(1)) if num_eval_points else None
 
-            # extract crps
-            crps = re.search(r'CRPS: (\d+\.\d+|\d+)', text)
-            crps = float(crps.group(1)) if crps else None
+for p_missing in p_missing_list:
+    print(f'p_missing: {p_missing}')
+    deer_id_list = os.listdir(f'./results/{p_missing}')
 
 
 
+    # remove .DS_Store
+    deer_id_list = [int(x) for x in deer_id_list if x != '.DS_Store']
+    deer_list = []
+    mae_interpolation_list = []
+    mae_transformer_list = []
+    mae_csdi_list = []
+    num_eval_points_list = []
+    crps_list = []
+
+    for deer_id in sorted(deer_id_list):
+        # print(deer_id)
+
+        # enter the interpolation folder, and read mae.txt file
+        try:
+            with open(f'./results/{deer_id}/interpolation/mae.txt') as f:
+                text = f.read()
+
+                # extract the mae value from 'Test MAE: 123.456\nTest MRE' using regex
+                specific_number = re.search(r'Test MAE: (\d+\.\d+|\d+)', text)
+                mae_interpolation = float(specific_number.group(1)) if specific_number else None
+
+
+            # with open(f'./results/{deer_id}/transformer/mae.txt') as f:
+            #     mae = f.read()
+            #     print(mae)
+
+            with open(f'./results/{deer_id}/csdi/mae.txt') as f:
+                text = f.read()
+                # extract the mae value from 'Test MAE: 123.456\nTest MRE' using regex
+                specific_number = re.search(r'Test MAE: (\d+\.\d+|\d+)', text)
+                mae_csdi = float(specific_number.group(1)) if specific_number else None
+                num_eval_points = re.search(r'Number of evaluated data points: (\d+)', text)
+                num_eval_points = int(num_eval_points.group(1)) if num_eval_points else None
+
+                # extract crps
+                crps = re.search(r'CRPS: (\d+\.\d+|\d+)', text)
+                crps = float(crps.group(1)) if crps else None
 
 
 
-        deer_list.append(deer_id)
-        mae_interpolation_list.append(mae_interpolation)
-        mae_csdi_list.append(mae_csdi)
-        num_eval_points_list.append(num_eval_points)
-        crps_list.append(crps)
-
-    except:
-        pass
 
 
-# make a dataframe using deer_list, mae_interpolation_list, mae_csdi_list
-df = pd.DataFrame({'deer_id': deer_list, 'mae_interpolation': mae_interpolation_list, 'mae_csdi': mae_csdi_list, 'num_eval_points': num_eval_points_list, 'crps': crps_list})
 
-# count how many times mae_csdi is smaller than mae_interpolation, and average decrease in mae
+            deer_list.append(deer_id)
+            mae_interpolation_list.append(mae_interpolation)
+            mae_csdi_list.append(mae_csdi)
+            num_eval_points_list.append(num_eval_points)
+            crps_list.append(crps)
 
-count = 0
-for i in range(len(df)):
-    if float(df['mae_csdi'][i]) < float(df['mae_interpolation'][i]):
-        count += 1
-
-# print the count
-print('CSDI is better than interpolation:', count, 'out of', len(df), 'times')
+        except:
+            pass
 
 
-df['mae_interpolation_total'] = df['mae_interpolation'] * df['num_eval_points']
-df['mae_csdi_total'] = df['mae_csdi'] * df['num_eval_points']
+    # make a dataframe using deer_list, mae_interpolation_list, mae_csdi_list
+    df = pd.DataFrame({'deer_id': deer_list, 'mae_interpolation': mae_interpolation_list, 'mae_csdi': mae_csdi_list, 'num_eval_points': num_eval_points_list, 'crps': crps_list})
 
-df['mae_total_diff']=  df['mae_interpolation_total'] - df['mae_csdi_total']
+    # count how many times mae_csdi is smaller than mae_interpolation, and average decrease in mae
 
-print('mae for CSDI:', df['mae_csdi_total'].sum() / df['num_eval_points'].sum())
-print('mae for interpolation:', df['mae_interpolation_total'].sum() / df['num_eval_points'].sum())
-print('Mean of the difference between mae_interpolation and mae_csdi:', df['mae_total_diff'].sum() / df['num_eval_points'].sum())
+    count = 0
+    for i in range(len(df)):
+        if float(df['mae_csdi'][i]) < float(df['mae_interpolation'][i]):
+            count += 1
 
-print('Mean of CRPS:', df['crps'].sum() / len(df))
+    # print the count
+    print('CSDI is better than interpolation:', count, 'out of', len(df), 'times')
 
-# save the dataframe to a csv file
-df.to_csv('results.csv', index=False)
+
+    df['mae_interpolation_total'] = df['mae_interpolation'] * df['num_eval_points']
+    df['mae_csdi_total'] = df['mae_csdi'] * df['num_eval_points']
+
+    df['mae_total_diff']=  df['mae_interpolation_total'] - df['mae_csdi_total']
+
+    print('mae for CSDI:', df['mae_csdi_total'].sum() / df['num_eval_points'].sum())
+    print('mae for interpolation:', df['mae_interpolation_total'].sum() / df['num_eval_points'].sum())
+    print('Mean of the difference between mae_interpolation and mae_csdi:', df['mae_total_diff'].sum() / df['num_eval_points'].sum())
+
+    print('Mean of CRPS:', df['crps'].sum() / len(df))
+
+    # save the dataframe to a csv file
+    df.to_csv(f'./results/{p_missing}/summary.csv', index=False)
